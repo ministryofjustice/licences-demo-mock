99c0245deee23f65970bb061030b5c4ae859c1f5
mattwhitfield <whitfield.mj@gmail.com>
2018-10-02 14:58:19 +0100

    added image

--- routes/image.js
+++ routes/image.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
-router.get('/:bookingId', function(req, res) {
+router.get('/:imageId', function(req, res) {
     res.send({
         "imageId": 1,
         "captureDate": "2017-07-05",
@@ -11,6 +12,11 @@ router.get('/:bookingId', function(req, res) {
         "objectId": 0,
         "imageData": "data"
     })
+});
+
+router.get('/:imageId/data', function(req, res) {
+    const src = fs.createReadStream('routes/images/jim.png');
+    src.pipe(res);
 });
 
 module.exports = router;
