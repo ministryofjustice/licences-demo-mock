5ade774b18a18ad685557bd795557e0f9cde7a38
Peter Phillips <peter@greenthistle.co.uk>
2018-10-16 10:52:26 +0100

    NN-1200: Grab username from auth token and use that instead

--- routes/users.js
+++ routes/users.js
@@ -2,85 +2,92 @@ const express = require('express');
 const jwtDecode = require('jwt-decode');
 const router = express.Router();
 const profiles = {
-  ca: {
+  CA_USER: {
     "firstName": "Catherine",
     "lastName": "Amos",
     "staffId": "100",
     "email": "CA-DEMO@work",
     "activeCaseLoadId": "CA-DEMO",
   },
-  catest: {
+  CA_USER_TEST: {
     "firstName": "User",
     "lastName": "CA_USER",
     "staffId": "2",
     "email": "CA_USER@work",
     "activeCaseLoadId": "DEF",
   },
-  camulti: {
+  CA_USER_MULTI: {
     "firstName": "User",
-    "lastName": "CA_MULTI_USER",
+    "lastName": "CA_USER_MULTI",
     "staffId": "2",
     "email": "CA_USER@work",
     "activeCaseLoadId": "DEF",
   },
-  ro: {
+  RO_DEMO: {
     "firstName": "Ryan",
     "lastName": "Orton",
     "staffId": "1",
     "email": "RO_USER@work",
     "activeCaseLoadId": "ABC",
   },
-  rotest: {
+  RO_USER_TEST: {
     "firstName": "User",
     "lastName": "RO_USER",
     "staffId": "2",
     "email": "RO_USER@work",
     "activeCaseLoadId": "ABC",
   },
-  romulti: {
+  RO_USER_MULTI: {
     "firstName": "User",
-    "lastName": "ROMULTI_USER",
+    "lastName": "ROUSER_MULTI",
     "staffId": "1",
     "email": "RO_USER@work",
     "activeCaseLoadId": "ABC",
   },
-  dm: {
-    "firstName": "Dianne",
-    "lastName": "Matthews",
-    "staffId": "3",
-    "email": "DM_USER@work",
-    "activeCaseLoadId": "GHI",
+  RO_USER: {
+    "firstName": "Ryan",
+    "lastName": "Orton",
+    "staffId": "1",
+    "email": "RO_USER@work",
+    "activeCaseLoadId": "ABC",
   },
-  dmtest: {
+  DM_USER_TEST: {
     "firstName": "User",
     "lastName": "DM_USER",
     "staffId": "3",
     "email": "DM_USER@work",
     "activeCaseLoadId": "GHI",
   },
-  dmmulti: {
+  DM_USER_MULTI: {
     "firstName": "User",
-    "lastName": "DMMULTI_USER",
+    "lastName": "DMUSER_MULTI",
     "staffId": "3",
     "email": "DM_USER@work",
     "activeCaseLoadId": "GHI",
-  }
+  },
+  DM_USER: {
+    "firstName": "Dianne",
+    "lastName": "Matthews",
+    "staffId": "3",
+    "email": "DM_USER@work",
+    "activeCaseLoadId": "GHI",
+  },
 };
 
 const roles = {
-  ca: {
+  CA: {
     "roleId": 0,
     "roleName": "string",
     "roleCode": "LEI_LICENCE_CA",
     "parentRoleCode": "string",
   },
-  ro: {
+  RO: {
     "roleId": 0,
     "roleName": "string",
     "roleCode": "LEI_LICENCE_RO",
     "parentRoleCode": "string",
   },
-  dm: {
+  DM: {
     "roleId": 0,
     "roleName": "string",
     "roleCode": "LEI_LICENCE_DM",
@@ -113,15 +120,12 @@ function findFirstFromToken(token, roleHash, type = 'PROFILE') {
   try {
     // try for a real jwt to get the roles from
     const jwt = jwtDecode(accessToken);
-    const authorities = jwt.authorities;
-    return authorities.map(roleCode => roleHash[roleCode.slice(-2).toLowerCase()]).find(a => a);
+    const lookup = type === 'ROLE' ? jwt.user_name.substring(0, 2) : jwt.user_name;
+    return roleHash[lookup];
   } catch (error) {
     // otherwise fallback to a ca_token, ro_token, dm_token
-    if (type === 'ROLE') {
-      return roleHash[accessToken.substring(0, 2).toLowerCase()];
-    }
-
-    return roleHash[accessToken.split('-')[0].toLowerCase()];
+    const lookup = type === 'ROLE' ? accessToken.substring(0, 2) : accessToken.replace('-token', '');
+    return roleHash[lookup];
   }
 }
 
