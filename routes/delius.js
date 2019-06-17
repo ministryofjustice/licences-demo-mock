78d420e255347c610f32d1eac13e813ff3524669
atodd-noms <alistair.todd@digital.justice.gov.uk>
2019-06-17 09:00:54 +0100

    Remove query param

--- routes/delius.js
+++ routes/delius.js
@@ -4,7 +4,6 @@ const router = express.Router()
 
 router.get('/staff/staffCode/:staffCode/managedOffenders', (req, res) => {
   const { staffCode } = req.params
-  const { current } = req.query
 
   const offenders = [
     {
@@ -13,9 +12,9 @@ router.get('/staff/staffCode/:staffCode/managedOffenders', (req, res) => {
       nomsNumber: 'A5001DY',
       crnNumber: 1234567,
       offenderSurname: 'Andrews',
-      isCurrentRo: current,
-      isCurrentOm: current,
-      isCurrentPom: current,
+      isCurrentRo: true,
+      isCurrentOm: true,
+      isCurrentPom: true,
       omStartDate: '01/01/2001',
       omEndDate: '01/01/2001',
     },
@@ -26,7 +25,6 @@ router.get('/staff/staffCode/:staffCode/managedOffenders', (req, res) => {
 
 router.get('/offenders/nomsNumber/:nomsNumber/responsibleOfficers', (req, res) => {
   const { nomsNumber } = req.params
-  const { current } = req.query
   const ros = [
     {
       nomsNumber,
@@ -42,9 +40,9 @@ router.get('/offenders/nomsNumber/:nomsNumber/responsibleOfficers', (req, res) =
       lduDescription: 'lduDescription',
       probationAreaCode: 'probationAreaCode',
       probationAreaDescription: 'probationAreaDescription',
-      isCurrentRo: current,
-      isCurrentOm: current,
-      isCurrentPom: current,
+      isCurrentRo: true,
+      isCurrentOm: true,
+      isCurrentPom: true,
       omStartDate: '01/01/2001',
       omEndDate: '01/01/2001',
     },
