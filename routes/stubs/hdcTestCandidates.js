bb0f5afa4f61db44c4bd5c8486802f77360e6e41
atodd-noms <alistair.todd@digital.justice.gov.uk>
2019-06-26 14:24:07 +0100

    Add sentence start date

--- routes/stubs/hdcTestCandidates.js
+++ routes/stubs/hdcTestCandidates.js
@@ -15,6 +15,7 @@ module.exports = [
       homeDetentionCurfewEligibilityDate: '2019-08-23',
       homeDetentionCurfewActualDate: '2019-09-13',
       automaticReleaseDate: '2020-02-02',
+      sentenceStartDate: '2010-05-24',
       sentenceExpiryDate: '2020-05-24',
       licenceExpiryDate: '2020-05-02',
       topupSupervisionExpiryDate: '2020-10-15',
